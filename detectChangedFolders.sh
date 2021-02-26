10cfbaf3c933cd39a7bf145ae75092d4092743f7
JSDKTEAM <jetsadakorn1@windowslive.com>
2021-02-26 18:20:52 +0700

    Update detectChangedFolders.sh

--- detectChangedFolders.sh
+++ detectChangedFolders.sh
@@ -4,4 +4,15 @@
 # echo $folders
 
 changed_folders=`git diff --name-only HEAD~1 | grep / | awk 'BEGIN {FS="/"} {print $1}' | uniq`
+for folder in $changed_folders
+ do
+   if [ "$folder" == '_global' ]; then
+     echo "common folder changed, building and publishing all microservices"
+     changed_services=`find . -maxdepth 1 -type d -not -name '_global' -not -name 'shippable' -not -name '.git' -not -path '.' | sed 's|./||'`
+     echo "list of microservice "$changed_services
+     break
+   else
+     echo "Adding $folder to list of services to build"
+   fi
+ done
 echo "changed folders "$changed_folders
