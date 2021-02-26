1282ca055b0b9c3961f5bc332be04fde8515eb8b
JSDKTEAM <jetsadakorn1@windowslive.com>
2021-02-26 18:04:49 +0700

    test

--- detectChangedFolders.sh
+++ detectChangedFolders.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -e
 
 folders=$(git --no-pager diff --name-only HEAD~1 | sort -u | awk 'BEGIN {FS="/"} {print $1}' | uniq); 
-echo $folders
+for j in "${folders[@]}"
+    do
+        echo $j
+        if [[ $i = $j ]]; then
+            SHOULD_BUILD=1 
+        fi
+done
